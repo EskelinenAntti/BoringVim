404740e5653a6b05e17867b38ad8d9c86b1f4a68
Antti Eskelinen <29664167+EskelinenAntti@users.noreply.github.com>
2025-08-23 08:11:30 +0300

    something

--- lua/boringvim/init.lua
+++ lua/boringvim/init.lua
@@ -19,7 +19,7 @@ autocmd('LspAttach', {
         local opts = { buffer = e.buf }
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
         vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "<leader>h", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
         vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
         vim.keymap.set("n", "<leader>va", function() vim.lsp.buf.code_action() end, opts)
         vim.keymap.set("n", "<leader>vr", function() vim.lsp.buf.rename() end, opts)
